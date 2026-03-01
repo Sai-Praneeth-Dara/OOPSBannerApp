211c10675562fbcd2b091ae6ff3ebecb142bcde5
sai_praneeth <dsaipraneeth28@gmail.com>
2026-03-01 18:28:58 +0530

    Added UC5: Inline array

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,28 +1,25 @@
 /**
- * OOPSBannerApp UC4 – Render OOPS using String Array and Loop
+ * OOPSBannerApp
  *
- * Improves UC3 by storing banner lines in a String array
- * and printing them using a for-each loop.
+ * UC5: Render OOPS as Banner using Inline Array Initialization
  *
  * @author Sai Praneeth Dara
- * @version 4.0
+ * @version 5.0
  */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
+        String[] lines = {
+            String.join(" ", "  ***     ", "  ***     ", " *****   ", " ***** "),
+            String.join(" ", " ** **    ", " ** **    ", " **  **  ", " **    "),
+            String.join(" ", "**   **   ", "**   **   ", " **   ** ", " **    "),
+            String.join(" ", "**   **   ", "**   **   ", " *****   ", " ***   "),
+            String.join(" ", "**   **   ", "**   **   ", " **      ", "   **  "),
+            String.join(" ", " ** **    ", " ** **    ", " **      ", "   **  "),
+            String.join(" ", "  ***     ", "  ***     ", " **      ", " ***** ")
+        };
 
-        lines[0] = String.join("", "  *****   ", "  *****   ", " ******** ", " *******  ");
-        lines[1] = String.join("", " **   **  ", " **   **  ", " **     **", " **     **");
-        lines[2] = String.join("", " **   **  ", " **   **  ", " **     **", " **       ");
-        lines[3] = String.join("", " **   **  ", " **   **  ", " ******** ", "  ******  ");
-        lines[4] = String.join("", " **   **  ", " **   **  ", " **       ", "       ** ");
-        lines[5] = String.join("", " **   **  ", " **   **  ", " **       ", " **    ** ");
-        lines[6] = String.join("", "  *****   ", "  *****   ", " **       ", "  ******  ");
-
-        // Loop to print
         for (String line : lines) {
             System.out.println(line);
         }
