001c0808a1e91a4c0a488f5f741f354ae8aca84e
sai_praneeth <dsaipraneeth28@gmail.com>
2026-03-01 19:17:57 +0530

    Added UC6:StaticFunction

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,68 @@
 /**
  * OOPSBannerApp
  *
- * UC5: Render OOPS as Banner using Inline Array Initialization
+ * UC6: Render OOPS Banner using Static Helper Methods
  *
- * @author Sai Praneeth Dara
- * @version 5.0
+ * @author Sai Praneeth Dara 
+ * @version 6.0
  */
 public class OOPSBannerApp {
 
+    // Method to generate pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[] {
+            "  ***   ",
+            " ** **  ",
+            "**   ** ",
+            "**   ** ",
+            "**   ** ",
+            " ** **  ",
+            "  ***   "
+        };
+    }
+
+    // Method to generate pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[] {
+            " *****  ",
+            " **  ** ",
+            " **   **",
+            " *****  ",
+            " **     ",
+            " **     ",
+            " **     "
+        };
+    }
+
+    // Method to generate pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[] {
+            " ***** ",
+            " **    ",
+            " **    ",
+            " ***   ",
+            "   **  ",
+            "   **  ",
+            " ***** "
+        };
+    }
+
+    // Main method
     public static void main(String[] args) {
 
-        String[] lines = {
-            String.join(" ", "  ***     ", "  ***     ", " *****   ", " ***** "),
-            String.join(" ", " ** **    ", " ** **    ", " **  **  ", " **    "),
-            String.join(" ", "**   **   ", "**   **   ", " **   ** ", " **    "),
-            String.join(" ", "**   **   ", "**   **   ", " *****   ", " ***   "),
-            String.join(" ", "**   **   ", "**   **   ", " **      ", "   **  "),
-            String.join(" ", " ** **    ", " ** **    ", " **      ", "   **  "),
-            String.join(" ", "  ***     ", "  ***     ", " **      ", " ***** ")
-        };
+        // Get patterns using methods
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        for (String line : lines) {
-            System.out.println(line);
+        // Print banner line by line
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + " " +
+                oPattern[i] + " " +
+                pPattern[i] + " " +
+                sPattern[i]
+            );
         }
     }
 }
